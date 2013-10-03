91aedd82fd13004224532cba1e9c8c60a08d919e
App Academy Student <student@appacademy.io>
2013-10-03 11:15:38 -0700

    Empty Project

---
+++ Polls/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Polls::Application.config.secret_token = 'd733f2a271c143e8288c815ee93beb152d5566d3e6eeb81d32d861cfcc52b78a17f776ca7f43dccd74f043912ba6193d03894a51d94d936acc22bebc0d3c0e0d'
